86a51ddf6b94dc911cd61ade5fd6bcd3ad74dd04
Hélio <heliorpjunior116@gmail.com>
2023-12-11 15:55:40 -0300

    feat: criação do serviço de deletar um produto

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,24 @@
+import { inject, injectable } from 'tsyringe';
+import { IProductsRepository } from '../domain/repositories/IProductsRepository';
+import { IDeleteProduct } from '../domain/models/IDeleteProduct';
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+class DeleteProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({ id }: IDeleteProduct): Promise<void> {
+    const product = await this.productsRepository.findById(id);
+
+    if (!product) {
+      throw new AppError('Product not found.', 400);
+    }
+
+    await this.productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;

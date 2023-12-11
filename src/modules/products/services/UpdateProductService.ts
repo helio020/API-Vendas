bd316f480b9cd1265177fa44f296b072b42cbbaa
Hélio <heliorpjunior116@gmail.com>
2023-12-11 15:51:00 -0300

    feat: criação do serviço de atualizar um produto

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import { inject, injectable } from 'tsyringe';
+import { IProductsRepository } from '../domain/repositories/IProductsRepository';
+import { IUpdateProduct } from '../domain/models/IUpdateProduct';
+import { IProduct } from '../domain/models/IProduct';
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+class UpdateProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IUpdateProduct): Promise<IProduct> {
+    const product = await this.productsRepository.findById(id);
+
+    if (!product) {
+      throw new AppError('Product not found.', 400);
+    }
+
+    const productExists = await this.productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name', 400);
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await this.productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;

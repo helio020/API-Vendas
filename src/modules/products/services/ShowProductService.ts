59fd68dffdbdf4f726a06fe9ec309ee75e0f823c
Hélio <heliorpjunior116@gmail.com>
2023-12-11 15:50:39 -0300

    feat: criação do serviço de mostrar um produto

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'tsyringe';
+import { IProductsRepository } from '../domain/repositories/IProductsRepository';
+import { IShowProduct } from '../domain/models/IShowProduct';
+import { IProduct } from '../domain/models/IProduct';
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+class ShowProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({ id }: IShowProduct): Promise<IProduct> {
+    const product = await this.productsRepository.findById(id);
+
+    if (!product) {
+      throw new AppError('Product not found.', 400);
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;

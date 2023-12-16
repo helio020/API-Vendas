08a7f6a771d32c15874895e246f762d4407e519f
Hélio Júnior <heliorpjunior116@gmail.com>
2023-12-16 12:49:51 -0300

    feat

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import { errors } from 'celebrate';
+import '@shared/infra/typeorm';
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, request: Request, response: Response) => {
   if (error instanceof AppError) {

bf2e26d24943394cb1158b1e1bb32797e26f31df
Hélio Júnior <heliorpjunior116@gmail.com>
2023-12-19 19:12:41 -0300

    feat: criação do block try/catch

--- src/modules/products/infra/http/controllers/ProductsController.ts
+++ src/modules/products/infra/http/controllers/ProductsController.ts
@@ -10,49 +10,77 @@ export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const page = request.query.page ? Number(request.query.page) : 1;
     const limit = request.query.limit ? Number(request.query.limit) : 15;
-    const listProducts = container.resolve(ListProductService);
-    const products = await listProducts.execute({ page, limit });
-    return response.json(products);
+
+    try {
+      const listProducts = container.resolve(ListProductService);
+
+      const products = await listProducts.execute({ page, limit });
+
+      return response.json(products);
+    } catch (error) {
+      return response.status(400).json(error);
+    }
   }
 
   public async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
 
-    const showProduct = container.resolve(ShowProductService);
+    try {
+      const showProduct = container.resolve(ShowProductService);
 
-    const product = await showProduct.execute({ id });
+      const product = await showProduct.execute({ id });
 
-    return response.json(product);
+      return response.json(product);
+    } catch (error) {
+      return response.status(400).json(error);
+    }
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body;
 
-    const createProduct = container.resolve(CreateProductService);
+    try {
+      const createProduct = container.resolve(CreateProductService);
 
-    const product = await createProduct.execute({ name, price, quantity });
+      const product = await createProduct.execute({ name, price, quantity });
 
-    return response.json(product);
+      return response.json(product);
+    } catch (error) {
+      return response.status(400).json(error);
+    }
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body;
     const { id } = request.params;
 
-    const updateProduct = container.resolve(UpdateProductService);
+    try {
+      const updateProduct = container.resolve(UpdateProductService);
 
-    const product = await updateProduct.execute({ id, name, price, quantity });
+      const product = await updateProduct.execute({
+        id,
+        name,
+        price,
+        quantity,
+      });
 
-    return response.json(product);
+      return response.json(product);
+    } catch (error) {
+      return response.status(400).json(error);
+    }
   }
 
   public async delete(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
 
-    const deleteProduct = container.resolve(DeleteProductService);
+    try {
+      const deleteProduct = container.resolve(DeleteProductService);
 
-    await deleteProduct.execute({ id });
+      await deleteProduct.execute({ id });
 
-    return response.json([]);
+      return response.json([]);
+    } catch (error) {
+      return response.status(400).json(error);
+    }
   }
 }

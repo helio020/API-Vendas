a1121b705fd15b78c4ddd6e272cb7932447aa536
Hélio Júnior <heliorpjunior116@gmail.com>
2024-01-04 15:35:44 -0300

    feat: implementação da entidade de usuário

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,41 @@
+import { IUser } from '@modules/users/domain/models/IUser';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
+
+@Entity('users')
+class User implements IUser {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    return this.avatar ? `${process.env.BASE_AVATAR_URL}/${this.avatar}` : null;
+  }
+}
+
+export default User;

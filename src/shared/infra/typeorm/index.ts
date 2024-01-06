a3c83897f00d4a00fc7415c5b753d1e3ea5c8f32
Hélio Júnior <heliorpjunior116@gmail.com>
2024-01-06 16:33:55 -0300

    feat: instanciação e importação da entidade de token do usuário além da migração

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,8 @@ import { CreateProducts1702493784655 } from './migrations/1702493784655-CreatePr
 import Product from '@modules/products/infra/typeorm/entities/Product';
 import { CreateUsers1704388559610 } from './migrations/1704388559610-CreateUsers';
 import User from '@modules/users/infra/typeorm/entities/User';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+import { CreateUserTokens1704568884569 } from './migrations/1704568884569-CreateUserTokens';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -11,6 +13,10 @@ export const dataSource = new DataSource({
   username: 'postgres',
   password: '43710',
   database: 'apivendas',
-  entities: [Product, User],
-  migrations: [CreateProducts1702493784655, CreateUsers1704388559610],
+  entities: [Product, User, UserToken],
+  migrations: [
+    CreateProducts1702493784655,
+    CreateUsers1704388559610,
+    CreateUserTokens1704568884569,
+  ],
 });

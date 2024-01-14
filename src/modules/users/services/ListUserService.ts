a77d2e21e840b69e766690684a380fb62d4a3024
Hélio Júnior <heliorpjunior116@gmail.com>
2024-01-14 15:39:34 -0300

    feat: implementação do serviço de listar usuários

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe';
+import UsersRepository from '../infra/typeorm/repositories/UsersRepository';
+import { IUsersRepository } from '../domain/repositories/IUsersRepository';
+import { IPaginateUser } from '../domain/models/IPaginateUser';
+
+interface SearchParams {
+  page: number;
+  limit: number;
+}
+
+@injectable()
+class ListUserService {
+  constructor(
+    @inject(UsersRepository)
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ page, limit }: SearchParams): Promise<IPaginateUser> {
+    const take = limit;
+    const skip = (Number(page) - 1) * take;
+    const users = await this.usersRepository.findAll({
+      page,
+      skip,
+      take,
+    });
+
+    return users;
+  }
+}
+
+export default ListUserService;

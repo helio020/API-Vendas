11a03f2254bb5e3a19d2a966d9acfe4ba14d00d8
Hélio Júnior <heliorpjunior116@gmail.com>
2024-01-14 12:43:33 -0300

    feat: implementação do serviço de criação da sessão do usuário

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,47 @@
+import { inject, injectable } from 'tsyringe';
+import { IUsersRepository } from '../domain/repositories/IUsersRepository';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
+import { ICreateSession } from '../domain/models/ICreateSession';
+import { IUserAuthenticated } from '../domain/models/IUserAuthenticated';
+import AppError from '@shared/errors/AppError';
+import { Secret, sign } from 'jsonwebtoken';
+import auth from '@config/auth';
+
+@injectable()
+class CreateSessionsService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    email,
+    password,
+  }: ICreateSession): Promise<IUserAuthenticated> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 400);
+    }
+
+    const passwordConfirmed = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password combination', 400);
+    }
+
+    const token = sign({}, auth.jwt.secret as Secret, {
+      subject: user.id,
+      expiresIn: auth.jwt.expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default CreateSessionsService;

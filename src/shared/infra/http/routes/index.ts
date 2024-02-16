e764a465f380ff2c4ee2e6e282fbecac3aba5323
Hélio Júnior <heliorpjunior116@gmail.com>
2024-02-16 18:08:40 -0300

    feat: implementação das rotas de senhas do usuário

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/infra/http/routes/products.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
@@ -9,6 +10,7 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 
 export default routes;

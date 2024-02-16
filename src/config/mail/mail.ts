4225f80905098b916624f96a624b89e48c707ebe
Hélio Júnior <heliorpjunior116@gmail.com>
2024-02-16 17:05:17 -0300

    feat: implementação das configurações de email

---
+++ src/config/mail/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'dodo_cruzeiro@hotmail.com',
+      name: 'Hélio Ribeiro',
+    },
+  },
+} as IMailConfig;
